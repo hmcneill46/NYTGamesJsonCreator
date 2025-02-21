f0742019bfdc786c7165609e0f1bc862af4757ae
Harry McNeill <harryolivermcneill@gmail.com>
2025-02-21 00:09:03 +0000

    feat: start implimenting mini

---
+++ MiniCreator.py
@@ -0,0 +1,13 @@
+import random
+import datetime
+class MiniPuzzle:
+    def __init__(self, puzzle):
+        self.body = []
+        self.constructors = []
+        self.copyright = 
+        self.editor = 
+        self.id = random.randint(10000, 99999)
+        self.lastUpdated = datetime.datetime.now()
+        self.publicationDate = 
+        self.subcategory = 
+        self.freePuzzle = True
